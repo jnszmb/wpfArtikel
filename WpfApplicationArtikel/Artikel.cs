6458650d8eea0e1faa40d4b49dbfc6621639bdec
zimmermannjonas <zimmermannjonas@dv-schulen.de>
2018-04-16 12:39:20 +0200

    16.04.2018

---
+++ WpfApplicationArtikel/Artikel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplicationArtikel
+{
+    class Artikel
+    {
+        private String artNr;
+        private String artBez;
+        private int bestand;
+        private int bestellbestand;
+        private decimal einkaufspreis;
+        private decimal verkaufspreis;
+        private int bestellmenge;
+
+        public string ArtNr
+        {
+            get
+            {
+                return artNr;
+            }
+
+            set
+            {
+                artNr = value;
+            }
+        }
+
+        public string ArtBez
+        {
+            get
+            {
+                return artBez;
+            }
+
+            set
+            {
+                artBez = value;
+            }
+        }
+
+        public int Bestand
+        {
+            get
+            {
+                return bestand;
+            }
+
+            set
+            {
+                bestand = value;
+            }
+        }
+
+        public int Bestellbestand
+        {
+            get
+            {
+                return bestellbestand;
+            }
+
+            set
+            {
+                bestellbestand = value;
+            }
+        }
+
+        public decimal Einkaufspreis
+        {
+            get
+            {
+                return einkaufspreis;
+            }
+
+            set
+            {
+                einkaufspreis = value;
+            }
+        }
+
+        public decimal Verkaufspreis
+        {
+            get
+            {
+                return verkaufspreis;
+            }
+
+            set
+            {
+                verkaufspreis = value;
+            }
+        }
+
+        public int Bestellmenge
+        {
+            get
+            {
+                return bestellmenge;
+            }
+
+            set
+            {
+                bestellmenge = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}", this.ArtBez);
+        }
+
+        public int nachbestellen()
+        {
+            this.Bestand += Bestellmenge;
+            return this.Bestand;
+        }
+        public int verkaufen(int mng)
+        {
+            this.Bestand -= mng;
+            return this.Bestand;
+        }
+
+    }
+}
--- WpfApplicationArtikel/MainWindow.xaml.cs
+++ WpfApplicationArtikel/MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace WpfApplicationArtikel
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Artikel> alleArtikel = null;
         public MainWindow()
         {
+            alleArtikel = new List<Artikel>();
             InitializeComponent();
+            Artikel artikel1 = new Artikel { ArtNr = "100", ArtBez = "Tisch", Bestand = 50, Bestellbestand = 20, Bestellmenge = 50, Einkaufspreis = 50.00m, Verkaufspreis = 100.00m };
+            Artikel artikel2 = new Artikel { ArtNr = "200", ArtBez = "Sessel", Bestand = 20, Bestellbestand = 5, Bestellmenge = 20, Einkaufspreis = 150.00m, Verkaufspreis = 220.00m };
+            Artikel artikel3 = new Artikel { ArtNr = "300", ArtBez = "Lampe", Bestand = 100, Bestellbestand = 20, Bestellmenge = 70, Einkaufspreis = 60.00m, Verkaufspreis = 80.00m };
+            Artikel artikel4 = new Artikel { ArtNr = "400", ArtBez = "Sofa", Bestand = 15, Bestellbestand = 3, Bestellmenge = 10, Einkaufspreis = 300.00m, Verkaufspreis = 500.00m };
+            Artikel artikel5 = new Artikel { ArtNr = "500", ArtBez = "Regal", Bestand = 70, Bestellbestand = 25, Bestellmenge = 60, Einkaufspreis = 40.00m, Verkaufspreis = 50.00m };
+
+            alleArtikel.Add(artikel1);
+            alleArtikel.Add(artikel2);
+            alleArtikel.Add(artikel3);
+            alleArtikel.Add(artikel4);
+            alleArtikel.Add(artikel5);
+
+            combobox_artikel.ItemsSource = alleArtikel;
+            
+        }
+
+        private void button_kaufen_Click(object sender, RoutedEventArgs e)
+        {
+            int menge = Convert.ToInt32(textbox_menge.Text);
+            Artikel a = (Artikel)combobox_artikel.SelectedItem;
+            if(menge > a.Bestand)
+            {
+                a.Bestand = a.nachbestellen();
+                textblock_status.Text = "Bestand zu niedrig...Nachbestellung ausgeführt";
+                textbox_bestand.Text = a.Bestand.ToString();
+            }
+            else
+            {
+                a.verkaufen(menge);
+                textblock_status.Text = "Vielen Dank für Ihren Einkauf";
+                textbox_bestand.Text = a.Bestand.ToString();
+                menge = 0;
+                textbox_menge.Text = "";
+            }
         }
     }
 }
